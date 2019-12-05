3c1282b2f1e03d765e990296d8563b8b78f093c4
treejw <42428487+treejw@users.noreply.github.com>
2019-12-05 18:40:10 +0900

    update

--- codes/Equal_stacks.py
+++ codes/Equal_stacks.py
@@ -13,7 +13,6 @@ def check_equal(sum_h_list):
 
 
 def equalStacks(h1, h2, h3):
-    
     h_list = [h1, h2, h3]
     sum_h_list = [sum(h1), sum(h2), sum(h3)]
 
@@ -29,28 +28,17 @@ def equalStacks(h1, h2, h3):
                 sum_h_list[i] -= x
 
     return sum_h_list[0]
-
     
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n1N2N3 = input().split()
-
     n1 = int(n1N2N3[0])
-
     n2 = int(n1N2N3[1])
-
     n3 = int(n1N2N3[2])
-
     h1 = list(map(int, input().rstrip().split()))
-
     h2 = list(map(int, input().rstrip().split()))
-
     h3 = list(map(int, input().rstrip().split()))
-
     result = equalStacks(h1, h2, h3)
-
     fptr.write(str(result))
-
     fptr.close()
