aecec20d45f58b0a1ccbfdc6e6535519a727293e
treejw <42428487+treejw@users.noreply.github.com>
2020-01-27 21:09:49 +0900

    Update Merge_the_Tools.py

--- Hackerrank_codes/Merge_the_Tools.py
+++ Hackerrank_codes/Merge_the_Tools.py
@@ -1,17 +1,14 @@
 def merge_the_tools(string, k):
     for i in range(0, len(string), k):
-        t = string[i:i+k]
-        
-        # 문자열 목표 단축길이 (t -> u) : while문 탈출조건에 사용
-        u_len = len(set(t)) 
-
-        u, idx = t[0], 1
-        while len(u) != u_len:
-            u += t[idx] if not t[idx] in t[:idx] else ''
-            idx += 1
-        print(u)
+        s_split = string[i:i+k]
+        output = s_split[0]
+        for s in s_split[1:]:
+            if s in output:
+                continue
+            output += s
+        print(output)
 
         
-if __name__ == '__main__':
+        if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
