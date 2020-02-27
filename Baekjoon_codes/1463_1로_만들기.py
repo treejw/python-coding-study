28f36d8eb43465d8a7a091dccd47b958e87f5840
treejw <42428487+treejw@users.noreply.github.com>
2020-02-27 15:26:47 +0900

    Update 1463_1로_만들기.py

--- Baekjoon_codes/1463_1로_만들기.py
+++ Baekjoon_codes/1463_1로_만들기.py
@@ -6,7 +6,6 @@ if __name__ == '__main__':
     N = int(input_func())
     DP_arr = [0] * (N + 1)
 
-    i = 1
     for i in range(1, N):
         if (i + 1 <= N) and (DP_arr[i + 1] == 0 or DP_arr[i + 1] > DP_arr[i] + 1):
             DP_arr[i + 1] = DP_arr[i] + 1
@@ -16,5 +15,5 @@ if __name__ == '__main__':
             DP_arr[i * 3] = DP_arr[i] + 1
 
     #                        1  2  3  4  5  6  7  8  9  10
-    # N = 10 -> DP_arr = [0, 0, 1, 1, 2, 3, 2, 0, 3, 2, 4]
+    # N = 10 -> DP_arr = [0, 0, 1, 1, 2, 3, 2, 3, 3, 2, 3]
     print(DP_arr[N])
